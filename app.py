4a2a5b454895c7c5c7d85361deebd970037e1a35
vaibhav-108 <vaibhav.b108@gmail.com>
2024-03-03 14:35:08 +0100

    main.yamnl

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def predict():
 
 if __name__ == '__main__':
     clapp = ClientApp()
-    app.run(host='0.0.0.0', port=8000,debug=True)
+    app.run(host='0.0.0.0', port=8080,debug=True)
     
     
     
